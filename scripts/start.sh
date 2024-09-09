d0a0215b338147f0dd8d3a87c296729e0253bc2a
Kozo Nishida <kozo.nishida@gmail.com>
2024-09-10 03:38:42 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@ R -e "library(dplyr)"
 
 R -e "plumber::pr_run(plumber::pr('/srv/app/plumber.R'), port=9000, host='0.0.0.0')" &
 
-R -e "shiny::runApp('/srv/app', host='0.0.0.0', port=1028)"
+R -e "shiny::runApp('/srv/app', host='0.0.0.0', port=7860)"
